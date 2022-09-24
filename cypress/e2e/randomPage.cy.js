f0a733be5ae434e0af563a3b9d04da7ebbc31ca9
Alex Souza <alex.webdevelloper@gmail.com>
2022-09-24 14:27:18 -0300

    test: Implements e2e tests with cypress

---
+++ cypress/e2e/randomPage.cy.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+const BASE_URL = 'http://localhost:3001/random';
+
+beforeEach(() => cy.visit(BASE_URL));
+
+describe('Random page tests', ()=>{
+
+    it('Should go to the random page when clicking at random button', async ()=>{
+
+        cy.get('a').contains('random').click();
+        cy.url().should('eq', `${BASE_URL}/random`);
+
+    });
+
+    it('Should upvote a video when clicking at upvote button', async ()=>{
+
+        cy.get('[data-cy-upvote]').click();
+        cy.get('[data-cy-upvote]').should('have.text', '1');
+
+    });
+
+    it('Should downvote a video when clicking at downvote button', async ()=>{
+
+        cy.get('[data-cy-downvote]').click();
+        cy.get('[data-cy-downvote]').should('have.text', '-1');
+
+    });
+
+});
---
+++ cypress/e2e/topPage.cy.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+const BASE_URL = 'http://localhost:3001/top';
+
+beforeEach(() => cy.visit(BASE_URL));
+
+describe('Top page tests', ()=>{
+
+    it('Should go to the top page when clicking at top button', async ()=>{
+
+        cy.get('a').contains('top').click();
+        cy.url().should('eq', `${BASE_URL}/top`);
+
+    });
+
+    it('Should upvote a video when clicking at upvote button', async ()=>{
+
+        cy.get('[data-cy-upvote]').click();
+        cy.get('[data-cy-upvote]').should('have.text', '1');
+
+    });
+
+    it('Should downvote a video when clicking at downvote button', async ()=>{
+
+        cy.get('[data-cy-downvote]').click();
+        cy.get('[data-cy-downvote]').should('have.text', '-1');
+
+    });
+
+});
